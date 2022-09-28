61fb92a14f717759cf887e187c9468acaaf17568
Soumyakant Swain <soumyakantswain5@gmail.com>
2022-09-28 23:29:38 +0530

    day-2

---
+++ Online-Cab-Booking-Application/src/main/java/com/masai/model/UserSession.java
@@ -0,0 +1,40 @@
+package com.masai.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSession {
+	
+	@Id
+	@SequenceGenerator(name="userSession_generator", sequenceName = "userSession_sequence", allocationSize=50)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userSession_generator")
+	private Integer sessionId;
+	
+	@Column(unique = true)
+	private Integer userId;
+	
+	private String uuid;
+	
+	@NotNull
+	private String userType;
+	private LocalDateTime sessionStartTime;
+	private LocalDateTime sessionEndTime;
+	
+	
+
+}
---
+++ Online-Cab-Booking-Application/src/main/java/com/masai/repository/UserSessionDao.java
@@ -0,0 +1,16 @@
+package com.masai.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.model.UserSession;
+
+@Repository
+public interface UserSessionDao extends JpaRepository<UserSession, Integer> {
+
+	public Optional<UserSession> findByUserId(Integer userId);
+	public Optional<UserSession> findByUuid(String  uuid);
+	
+}
