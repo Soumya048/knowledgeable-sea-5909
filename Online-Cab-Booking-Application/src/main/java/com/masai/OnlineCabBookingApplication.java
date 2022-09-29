95c7f840f6976e1904c4a2631f78c56124e78deb
Melbin Zacharia <102401711+melbinzaharia@users.noreply.github.com>
2022-09-29 19:09:37 +0530

    Update OnlineCabBookingApplication.java

--- Online-Cab-Booking-Application/src/main/java/com/masai/OnlineCabBookingApplication.java
+++ Online-Cab-Booking-Application/src/main/java/com/masai/OnlineCabBookingApplication.java
@@ -8,7 +8,7 @@ public class OnlineCabBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineCabBookingApplication.class, args);
-		System.out.println("Running.....");
+		
 	}
 
 }
