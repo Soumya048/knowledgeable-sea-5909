0c4f898bf8025a258bc4ed83943860c24a77fd21
Melbin Zacharia <102401711+melbinzaharia@users.noreply.github.com>
2022-09-29 18:32:59 +0530

    Add files via upload

---
+++ Online-Cab-Booking-Application/src/main/java/com/masai/service/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package com.masai.service;
+
+import java.util.List;
+
+import com.masai.exception.CustomerException;
+import com.masai.model.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public Customer insertCustomer(Customer customer)throws CustomerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomer(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer deleteCustomer(int customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> viewCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer viewCustomer(Integer customerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer validateCustomer(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
